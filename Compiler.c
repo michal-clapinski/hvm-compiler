7252b2cb4f441b0283ba4e8184ebbae1a3359222
Michał Cłapiński <michal.clapinski@codilime.com>
2018-09-12 16:05:32 +0200

    fixed literal generation

--- Compiler.c
+++ Compiler.c
@@ -4,8 +4,6 @@
 
 void compile(Exp _p_)
 {
-    int *a;
-
     switch(_p_->kind) {
         case is_EAdd:
         case is_ESub:
@@ -30,13 +28,10 @@ void compile(Exp _p_)
             printf("/");
             break;
         case is_EInt:
-            a = &(_p_->u.eint_.integer_);
-            if (*a > 9) {
-                *a -= 9;
+            for (int a = _p_->u.eint_.integer_; a; a /= 9) {
                 compile(_p_);
                 printf("9+");
-            } else
-                printf("%d", *a);
+            }
             break;
         default:
             assert(0);
